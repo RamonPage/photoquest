73a2673d26f91807364695ded60e10f36751884d
Vinicius Teles <vinicius@improveit.com.br>
2010-10-16 13:58:37 -0300

    Simplified Move

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,10 +1,10 @@
 class Move < CouchRest::Model::Base
   
-  property :quest_id, String
   property :answer, String
   
+  belongs_to :quest
+  
   def correct?
-    quest = Quest.get self.quest_id
-    quest.correct_answer?(self.answer)
+    self.quest.correct_answer?(self.answer)
   end
 end
