f6d70deb913dfac298eb37b3eb89160b6edf00af
Vinicius Teles <vinicius@improveit.com.br>
2010-10-16 02:31:57 -0300

    It's able to send an answer now.

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -10,5 +10,19 @@ describe ChallengesController do
     end
     
   end
+  
+  describe "POST move" do
+    
+    it "should return a move with incorrect answer" do
+      post :move, :id => "quest1", :answer => "Portland"
+      assigns(:move).correct.should be_false
+    end
+
+    it "should return a move with correct answer" do
+      post :move, :id => "quest1", :answer => "Chicago"
+      assigns(:move).correct.should be_true
+    end
+    
+  end
 
 end
