9040d3e8c89d1bdaf0412a7b0c4efcb3728fd96e
Vinicius Teles <vinicius@improveit.com.br>
2010-10-16 01:46:11 -0300

    Added Move model.

---
+++ spec/models/move_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Move do
+  
+  describe "properties" do
+    
+    it "should have the expected properties" do
+      move = Move.first
+      move.quest_id.should == "quest1"
+      move.correct.should == true
+    end
+    
+  end
+  
+end
