e767a74b6dbe58feb7eb25942aa9231b8f7a9c54
Vinicius Teles <vinicius@improveit.com.br>
2010-10-16 01:12:56 -0300

    Added link to page where image is.

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,6 +1,7 @@
 class Quest < CouchRest::Model::Base
   
   property :image_url, String
+  property :page_where_image_is, String
   property :questions, [String]
   property :answer, String
   
--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -13,6 +13,7 @@ describe Quest do
     it "should have the expected properties" do
       quest = Quest.first
       quest.image_url.should ==  "http://farm4.static.flickr.com/3081/2558637383_96104ccea4.jpg"
+      quest.page_where_image_is = "http://www.flickr.com/photos/viniciusteles/2558637383/in/set-72157605484089896/"
       quest.questions.should == %w(Portland Chicago Rio Paris London)
       quest.answer.should == "Chicago"
     end
