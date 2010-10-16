c9dfbe0c6f53e993bfd108f9cb79114cec80f07e
Vinicius Teles <vinicius@improveit.com.br>
2010-10-16 00:26:27 -0300

    First controller started

---
+++ config/initializers/couchdb.rb
@@ -0,0 +1,5 @@
+require 'couchrest/model'
+db_config = ConfigLoader.load('database')
+CouchServer = CouchRest::Server.new(db_config['server'])
+CouchServer.default_database = db_config['database']
+CouchRest::Model::Base.use_database CouchServer.default_database
