a1608098d9a5e958bbe7fd78385316dbb2b77e1c
Vitor Pellegrino <vitorp@gmail.com>
2010-10-17 19:26:37 -0200

    fix

--- spec/controllers/rankings_controller_spec.rb
+++ spec/controllers/rankings_controller_spec.rb
@@ -9,11 +9,4 @@ describe RankingsController do
     end
   end
 
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
 end
