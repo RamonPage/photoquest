73793dc6cb4c7badd03e705fd0f4dff7ce9f01bd
Vitor Pellegrino <vitorp@gmail.com>
2010-10-17 19:16:18 -0200

    Creating an entry to the ranking for the current player

--- app/models/player.rb
+++ app/models/player.rb
@@ -37,6 +37,20 @@ REDUCE
   view_by :score, :map => SCORE_MAP_FUNCTION, :reduce => REDUCE_FUNCTION
   view_by :ranking, :map => RANKING_MAP_FUNCTION, :reduce => REDUCE_FUNCTION
 
+
+  def self.ranking
+    all
+  end
+
+  def sign_up!(nick_name)
+    self.nick_name = nick_name 
+    self.save! 
+  end 
+
+  def already_signed_up?
+    !self.nick_name.blank?
+  end 
+
   def create_quest(params)
     @quest = Quest.create(params)
     self.create_sharing_move
