7a813a974f61cac856fe1e56e3d1686eb6a92d02
rafaeldx7 <rafaeldx7@gmail.com>
2011-01-20 21:02:43 -0200

    add logger

--- config/initializers/couchdb.rb
+++ config/initializers/couchdb.rb
@@ -2,4 +2,9 @@ require 'couchrest/model'
 db_config = ConfigLoader.load('database')
 CouchServer = CouchRest::Server.new(db_config['server'])
 CouchServer.default_database = db_config['database']
+
+Rails.logger.info(db_config)
+Rails.logger.info(CouchServer.default_database)
+Rails.logger.info(CouchServer)
+
 CouchRest::Model::Base.use_database CouchServer.default_database
