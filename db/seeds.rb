60f6b32e920c8fcc350e8233d82f81414c9fb9fd
rafaeldx7 <rafaeldx7@gmail.com>
2011-01-20 02:52:31 -0200

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,23 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Quest.create! :image_url => 'http://www.destinosdeviagem.com/wp-content/gallery/rio-janeiro/rio_de_janeiro_brazil.jpg',
+              :twitter_screen_name => :photoque_st,
+              :twitter_image_url => 'http://a1.twimg.com/profile_images/1146505937/photoquest-logo.png',
+              :correct_answer => 'Rio de Janeiro',
+              :incorrect_answer1 => 'Buenos Aires',
+              :incorrect_answer2 => 'Santiago de Chile',
+              :incorrect_answer3 => 'Quito',
+              :incorrect_answer4 => 'Lima',
+              :short_id => nil
+
+Quest.create! :image_url => 'http://blog.decolar.com/wp-content/uploads/2010/12/Obelisco-Buenos-Aires.jpg',
+              :twitter_screen_name => :photoque_st,
+              :twitter_image_url => 'http://a1.twimg.com/profile_images/1146505937/photoquest-logo.png',
+              :correct_answer => 'Buenos Aires',
+              :incorrect_answer1 => 'Johannesburg',
+              :incorrect_answer2 => 'Bogotá',
+              :incorrect_answer3 => 'Asunción',
+              :incorrect_answer4 => 'La Paz',
+              :short_id => nil
--- lib/tasks/db.rake
+++ lib/tasks/db.rake
@@ -7,14 +7,22 @@ namespace :db do
       Fixtures.reload
     end
   end
-  
+
   namespace :fixtures do
     desc "Dump quests from DB"
     task :dump_quests => :environment do
       Fixtures.dump_quests
     end
   end
-  
+
+  desc "Load the seed data from db/seeds.rb"
+  task :seed => :environment do
+    seed_file = File.join(Rails.root, 'db', 'seeds.rb')
+    puts seed_file
+    load(seed_file) if File.exist?(seed_file)
+    puts "Done."
+  end
+
   desc "Delete and recreate the database"
   task :setup => :environment do
     Fixtures.setup_db
